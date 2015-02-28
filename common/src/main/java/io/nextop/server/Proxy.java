752a48ba8a587de88008a9b777809c9701007995
Brien Colwell <xcolwell@gmail.com>
2015-02-28 01:36:48 -0800

    proxy

--- common/src/main/java/io/nextop/server/Proxy.java
+++ common/src/main/java/io/nextop/server/Proxy.java
@@ -1,11 +1,65 @@
 package io.nextop.server;
 
+import io.nextop.client.Wire;
 import io.nextop.client.node.Head;
+import io.nextop.client.node.nextop.NextopNode;
+import rx.Observer;
 
-public class Proxy {
+import javax.annotation.Nullable;
+import java.util.NoSuchElementException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
-    Head in;
-    Head out;
+public final class Proxy implements Observer<Wire> {
+
+
+    final Head in;
+    final NextopNode inNextop;
+    final Head out;
+
+    PublishWireFactory wireFactory;
+
+
+    public Proxy() {
+        // set up nodes
+        // set wireFactory into inNextop
+        // set active
+    }
+
+
+
+    void start() {
+
+//        in.defaultReceive().
+
+    }
+
+    void stop() {
+
+    }
+
+
+
+    /////// Observer ///////
+
+    @Override
+    public void onNext(Wire wire) {
+        wireFactory.onNext(wire);
+    }
+
+    @Override
+    public void onCompleted() {
+        wireFactory.onCompleted();
+        stop();
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        wireFactory.onError(e);
+        stop();
+    }
+
+
 
     // FIXME defaultSubscriber on the in
     // FIXME route to out
@@ -19,4 +73,35 @@ public class Proxy {
     // FIXME memcache
     // FIXME - if request had image layer pragmas, do downsampling
 
+
+    static final class PublishWireFactory implements Wire.Factory, Observer<Wire> {
+
+        final BlockingQueue<Wire> wires = new LinkedBlockingQueue<Wire>();
+
+
+        /////// WireFactory ///////
+
+        @Override
+        public Wire create(@Nullable Wire replace) throws InterruptedException, NoSuchElementException {
+            return wires.take();
+        }
+
+        /////// Observer ///////
+
+        @Override
+        public void onNext(Wire wire) {
+            boolean s = wires.offer(wire);
+            assert s;
+        }
+
+        @Override
+        public void onCompleted() {
+            // Do nothing
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            // Do nothing
+        }
+    }
 }
